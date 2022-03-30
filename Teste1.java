d3aa6657ab90fd5bf4fb602b1637a7c0e969ee40
Daniel Maximo <65917867+danielpmaximo@users.noreply.github.com>
2022-03-30 18:36:28 -0300

    Add files via upload

---
+++ Teste1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Teste1 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("login: ");
+        String login = keyboard.nextLine();
+
+        System.out.println("senha: ");
+        String senha = keyboard.nextLine();
+    }
+}
