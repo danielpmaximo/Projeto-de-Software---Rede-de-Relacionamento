e2960312522555cd1038a49e47468b00164fcf45
Daniel Maximo <65917867+danielpmaximo@users.noreply.github.com>
2022-04-05 16:34:43 -0300

    Create Func.java

---
+++ Func.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class Func {
+    Scanner scan = new Scanner(System.in);
+
+    String nome;
+    String login;
+    String senha;
+    String comando_usuario;
+    String nascimento;
+    String pessoa_mensagem;
+    String mensagem;
+    String nome_comunidade;
+    String assunto_comunidade;
+    String usuario_comunidade;
+    String rm_usuario_comunidade;
+    //int cmd_comunidade;
+    String entrar_comunidade;
+    String pesquisa;
+
+    // Funcionalidade 1
+    public void Nome(){
+        nome = scan.nextLine();
+        
+    }
+    public void Login(){
+        login = scan.nextLine();
+    }
+    public void Senha(){
+        senha = scan.nextLine();
+    }
+    
+    // Funcionalidade 2
+    public void Nascimento(){
+        nascimento = scan.nextLine();
+    }
+    public void Cmd_Usuario(){
+        comando_usuario = scan.nextLine();
+    }
+
+    //Funcionalidade 4
+    public void PessoaMensagem(){
+        pessoa_mensagem = scan.nextLine();
+    }
+    public void Mensagem(){
+        mensagem = scan.nextLine();
+    }
+
+    //Funcionalidade 5
+    public void NomeComunidade(){
+        nome_comunidade = scan.nextLine();
+    }
+    public void AssuntoComunidade(){
+        assunto_comunidade = scan.nextLine();
+    }
+    public void Add_Usuario_Comunidade(){
+        usuario_comunidade = scan.nextLine();
+    }
+    public void Rm_Usuario_Comunidade(){
+        rm_usuario_comunidade = scan.nextLine();
+    }
+
+    //Funcionalidade 6
+    public void EntrarComunidade(){
+        entrar_comunidade = scan.nextLine();
+    }
+
+    //Funcionalidade 7
+    public void Pesquisa(){
+        pesquisa = scan.nextLine();
+    }
+
+}
