ed16b002c4929ac739c76f3dfa45203dca6c8811
Daniel Maximo <65917867+danielpmaximo@users.noreply.github.com>
2022-04-06 17:07:22 -0300

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,17 @@
+public class Menu {
+
+    public void CriarMenu(){
+        System.out.println("------------------------------");
+        System.out.println("Seja bem vindo ao iFace");
+        System.out.println("Digite 0 para sair do programa");
+        System.out.println("Digite 1 para criar uma conta");
+        System.out.println("Digite 2 para editar seu perfil");
+        System.out.println("Digite 4 para enviar uma mensagem para outro usuario");
+        System.out.println("Digite 5 para criar ou gerenciar uma comunidade");
+        System.out.println("Digite 6 para entrar em uma comunidade");
+        System.out.println("Digite 7 para visualizar um perfil");
+        System.out.println("Digite 8 para deletar seu perfil");
+        System.out.println("Digite 9 para enviar uma mensagem no feed");
+        System.out.print("Seu comando: ");
+    }
+}
